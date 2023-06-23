e8f54b8f7cb834a2db45131133589163a729d775
Haziq Bangash <72706449+haziq-bangash@users.noreply.github.com>
2023-06-23 12:21:16 +0500

    minor bugs fixes

--- client/src/components/TodoList.vue
+++ client/src/components/TodoList.vue
@@ -62,121 +62,122 @@
 </template>
 
 <script setup>
-  import { onMounted, ref } from "vue";
-  import store from "@/store/store";
-  import router from "@/router";
-  import {
-    getTodos,
-    createTodo,
-    updateTodo,
-    deleteTodo,
-    deleteAllTodos,
-  } from "../services/api";
+import { onMounted, ref } from "vue";
+import store from "@/store/store";
+import router from "@/router";
+import {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+  deleteAllTodos,
+} from "../services/api";
 
-  const todoList = ref([]);
-  const newTodo = ref("");
+const todoList = ref([]);
+const newTodo = ref("");
 
-  onMounted(() => {
-    const storedUserData = localStorage.getItem("userData");
-    if (!storedUserData) {
-      router.push({ name: "Login" });
-      return;
-    }
-
-    try {
-      const userData = JSON.parse(storedUserData);
-      store.commit("setUser", userData);
-
-      // Check if the user state is already populated
-      if (!store.getters.user) {
-        console.log("User state not populated, initializing it with data from local storage")
-        // If not, initialize it with the data from local storage
-        store.commit("setUser", userData);
-      }
-
-      fetchTodos(userData.token, userData._id);
-    } catch (error) {
-      console.error("Error parsing user data:", error);
-      router.push({ name: "Login" });
-    }
-  });
-
-  const fetchTodos = async (token, userId) => {
-    try {
-      const res = await getTodos(token, userId);
-      todoList.value = res;
-    } catch (error) {
-      console.error("Error fetching todos:", error);
-      alert("Failed to fetch todos. Please try again later.");
-    }
-  };
-
-  const getCheckboxChecked = (completed) => {
-    return completed;
-  };
-
-  const addTodo = async () => {
-    if (!newTodo.value) return;
-
-    const todo = {
-      task: newTodo.value,
-      completed: false,
-      created_at: Date.now(),
-      completed_time: null,
-    };
-
-    try {
-      await createTodo(todo, store.getters.user.token, store.getters.user._id);
-      todoList.value.push(todo);
-      newTodo.value = "";
-      alert("Todo added successfully");
-    } catch (error) {
-      console.error("Error adding todo:", error);
-      alert("Failed to add todo. Please try again later.");
-    }
-  };
-
-  const checkCompleted = async (index) => {
-    const todo = todoList.value[index];
-    todo.completed = !todo.completed;
-
-    try {
-      await updateTodo(todo, store.getters.user.token);
-      alert("Todo updated successfully");
-    } catch (error) {
-      console.error("Error updating todo:", error);
-      alert("Failed to update todo. Please try again later.");
-    }
-  };
-
-  const deleteTodoTask = async (index) => {
-    try {
-      // console.log(store.getters.user.token);
-      await deleteTodo(todoList.value[index]._id, store.getters.user.token);
-      todoList.value.splice(index, 1);
-      alert("Todo deleted successfully");
-    } catch (error) {
-      console.error("Error deleting todo:", error);
-      alert("Failed to delete todo. Please try again later.");
-    }
-  };
-
-  const deleteAll = async () => {
-    try {
-      await deleteAllTodos(store.getters.user.token, store.getters.user._id);
-      todoList.value = [];
-      alert("All todos deleted successfully");
-    } catch (error) {
-      console.error("Error deleting todos:", error);
-      alert("Failed to delete todos. Please try again later.");
-    }
-  };
-
-  const logout = () => {
-    localStorage.removeItem("userData");
-    store.commit("setUser", null);
+onMounted(() => {
+  const storedUserData = localStorage.getItem("userData");
+  if (!storedUserData) {
     router.push({ name: "Login" });
+    return;
+  }
+
+  try {
+    const userData = JSON.parse(storedUserData);
+
+    // Check if the user state is already populated
+    if (!store.getters.user) {
+      console.log(
+        "User state not populated, initializing it with data from local storage"
+      );
+      // If not, initialize it with the data from local storage
+      store.commit("setUser", userData);
+    }
+
+    fetchTodos(userData.token, userData._id);
+  } catch (error) {
+    console.error("Error parsing user data:", error);
+    router.push({ name: "Login" });
+  }
+});
+
+const fetchTodos = async (token, userId) => {
+  try {
+    const res = await getTodos(token, userId);
+    todoList.value = res;
+  } catch (error) {
+    console.error("Error fetching todos:", error);
+    alert("Failed to fetch todos. Please try again later.");
+  }
+};
+
+const getCheckboxChecked = (completed) => {
+  return completed;
+};
+
+const addTodo = async () => {
+  if (!newTodo.value) return;
+
+  const todo = {
+    task: newTodo.value,
+    completed: false,
+    created_at: Date.now(),
+    completed_time: null,
   };
+
+  try {
+    await createTodo(todo, store.getters.user.token, store.getters.user._id);
+    todoList.value.push(todo);
+    newTodo.value = "";
+    alert("Todo added successfully");
+  } catch (error) {
+    console.error("Error adding todo:", error);
+    alert("Failed to add todo. Please try again later.");
+  }
+};
+
+const checkCompleted = async (index) => {
+  const todo = todoList.value[index];
+  todo.completed = !todo.completed;
+
+  try {
+    await updateTodo(todo, store.getters.user.token);
+    alert("Todo updated successfully");
+  } catch (error) {
+    console.error("Error updating todo:", error);
+    alert("Failed to update todo. Please try again later.");
+  }
+};
+
+const deleteTodoTask = async (index) => {
+  try {
+    // console.log(store.getters.user.token);
+    await deleteTodo(todoList.value[index]._id, store.getters.user.token);
+    todoList.value.splice(index, 1);
+    alert("Todo deleted successfully");
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+    alert("Failed to delete todo. Please try again later.");
+  }
+};
+
+const deleteAll = async () => {
+  try {
+    await deleteAllTodos(store.getters.user.token, store.getters.user._id);
+    todoList.value = [];
+    alert("All todos deleted successfully");
+  } catch (error) {
+    console.error("Error deleting todos:", error);
+    alert("Failed to delete todos. Please try again later.");
+  }
+};
+
+const logout = () => {
+  localStorage.removeItem("userData");
+  store.commit("setUser", null);
+  router.push({ name: "Login" });
+};
 </script>
 
 <style scoped>
